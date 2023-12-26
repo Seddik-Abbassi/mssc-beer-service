5f7c7e53582b5b5f84bc08dd70e0f89fc51b19b4
Seddik Abbassi <abbassi.seddik@gmail.com>
2023-12-26 20:39:22 +0100

    adding validation. Closes #8

--- mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/model/BeerDto.java
+++ mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/model/BeerDto.java
@@ -1,5 +1,9 @@
 package guru.springframework.msscbeerservice.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,18 +21,23 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+    @Null
     private UUID id;
+    @Null
     private int version;
-
+    @Null
     private OffsetDateTime createdDate;
+    @Null
     private OffsetDateTime lastModifiedDate;
-
+    @NotBlank
     private String beerName;
-
+    @NotNull
     private BeerStyleEnumm beerStyle;
-
+    @Positive
+    @NotNull
     private Long upc;
-
+    @Positive
+    @NotNull
     private BigDecimal price;
 
     private int quantityOnHand;
