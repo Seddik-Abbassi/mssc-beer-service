95a5405303e8ffe22326b92cacf016bac1056805
Seddik Abbassi <abbassi.seddik@gmail.com>
2023-12-29 14:36:33 +0100

    add jackson config to BeerDto. Closes #5

--- mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package guru.springframework.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
@@ -26,8 +27,10 @@ public class BeerDto {
 
     private Integer version;
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
     @NotBlank
     private String beerName;
@@ -36,6 +39,7 @@ public class BeerDto {
     @Positive
     @NotNull
     private Long upc;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
