7fbd8c9609a468d3e0a585402e93c4309103d3c3
Seddik Abbassi <abbassi.seddik@gmail.com>
2024-01-03 14:42:24 +0100

    implement ehcache

---
+++ mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/config/CacheConfig.java
@@ -0,0 +1,12 @@
+package guru.springframework.msscbeerservice.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by AD on 03/01/2024
+ */
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
--- mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/services/BeerServiceImpl.java
+++ mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/services/BeerServiceImpl.java
@@ -8,6 +8,7 @@ import guru.springframework.msscbeerservice.web.model.BeerDto;
 import guru.springframework.msscbeerservice.web.model.BeerPagedList;
 import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -24,8 +25,10 @@ public class BeerServiceImpl implements BeerService{
 
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
+    @Cacheable(cacheNames = "beerCache", key = "#beerId", condition = "#showInventoryOnHand == false ")
     @Override
     public BeerDto getById(UUID beerId, Boolean showInventoryOnHand) {
+        System.out.println("I was Called");
         BeerDto beerDto;
         if (showInventoryOnHand) {
             beerDto = beerMapper.beerToBeerDtoWithInventory(
@@ -52,10 +55,12 @@ public class BeerServiceImpl implements BeerService{
         return beerMapper.beerToBeerDto(beerRepository.save(beer));
     }
 
+    @Cacheable(cacheNames = "beerListCache", condition = "#showInventoryOnHand == false")
     @Override
     public BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest, Boolean showInventoryOnHand) {
         BeerPagedList beerPagedList;
         Page<Beer> beerPage;
+        System.out.println("I was Called");
 
         boolean isBeerNameNotBlank = beerName != null && !beerName.isBlank();
 
